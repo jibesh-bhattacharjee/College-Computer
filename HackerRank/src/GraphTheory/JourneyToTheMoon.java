1560e3468417e853aa71f46feb22e3f2876230ba
jibesh-bhattacharjee <jibesh.nitm@gmail.com>
2018-01-31 14:52:03 +0530

    JourneyToMoon problem --Test case 11 failed

--- HackerRank/src/GraphTheory/JourneyToTheMoon.java
+++ HackerRank/src/GraphTheory/JourneyToTheMoon.java
@@ -2,19 +2,26 @@ package GraphTheory;
 
 import java.util.ArrayList;
 import java.util.Scanner;
-
+/**
+ * @author Jibesh
+ * Result: Test case 11 not passed
+ */
 public class JourneyToTheMoon {
 
     static boolean[] visited;
     static ArrayList<Integer>[] adjList;
     static int connectedCmps;
+    static int astronautCnt;
+    static ArrayList<Integer> astroCountryCnt;
     
-    static void dfs(int node){
+    static int dfs(int node){
         visited[node] = true;
+        astronautCnt++;
         for(int v = 0; v < adjList[node].size(); v++){
             if(!visited[adjList[node].get(v)])
                 dfs(adjList[node].get(v));
         }
+        return astronautCnt;
     }
     
     static void addEdge(int a, int b){
@@ -25,24 +32,57 @@ public class JourneyToTheMoon {
     public static void main(String[] args) {
         //Utils
         Scanner in = new Scanner(System.in);
-        StringBuilder ans = new StringBuilder();
+        //StringBuilder ans = new StringBuilder();
         //Vars and Input
         int N = in.nextInt();
         int P = in.nextInt();
         
+        /**
+         * Instantiating adjList and visited,
+         * and also astroCountryCnt
+         */
+        visited = new boolean[N];
         adjList = new ArrayList[N];
-        for()
-        
+        for(int v=0;v<N;v++){
+            adjList[v] = new ArrayList();
+        }
+        astroCountryCnt = new ArrayList();
+        /**
+         * Filling up adjList
+         */
         for(int r=0;r<P;r++){
             int a = in.nextInt();
             int b = in.nextInt();
             
             addEdge(a,b);
         }
+        /**
+         * Finding number of connected components
+         * and the number of astronauts in them
+         */
+        for(int v=0;v<N;v++){
+            if(!visited[v]){
+                astronautCnt = 0;
+                dfs(v);
+                connectedCmps++;
+                astroCountryCnt.add(astronautCnt);
+                //System.out.println("The number of astronauts is: " + astronautCnt);
+            }
+        }
+        //System.out.println("The number of connected components is: " + connectedCmps);
         
-        for(int i=0;i<)
+        /**
+         * Number of astronauts and connected components works fine
+         */
+        
+        int sum = 0;
+        int result = 0;
         
         
+        for(Integer size : astroCountryCnt){
+            result += sum*size;
+            sum += size;
+        }
+        System.out.println(result);
     }
-    
 }
